d18c5d3a25d8a31a0ce5f458d69c8175ddd34fd3
Leont1cov <chuchunovvakov@mail.ru>
2026-03-17 19:45:17 +0300

    feat(api): setup axios instance with base config

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://api.poiskkino.dev',
+    baseURL: 'https://api.poiskkino.dev/v1.4',
     headers: {
         'Content-Type': 'application/json',
         'X-API-KEY': import.meta.env.VITE_KP_API_KEY,
