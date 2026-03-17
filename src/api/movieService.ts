8385d0d5d74e5afcf2e9c9574b5368230088db63
Leont1cov <chuchunovvakov@mail.ru>
2026-03-17 19:45:46 +0300

    feat(api): implement movie service with search filters

--- src/api/movieService.ts
+++ src/api/movieService.ts
@@ -2,15 +2,19 @@ import { api } from "./axiosInstance.ts";
 import type { MovieResponse } from "../types";
 
 export const movieService = {
-    getMovies: async (page = 1, limit = 10) => {
-        const data = await api.get<MovieResponse>('/movie', {
+    getMovies: async (page = 1, limit = 50) => {
+        const response = await api.get<MovieResponse>('/movie', {
             params: {
                 page,
                 limit,
+                'name': '!null',
                 'rating.kp': '7-10',
                 'poster.url': '!null',
+                'votes.kp': '1000-1000000',
+                'sortField': 'votes.kp',
+                'sortType': '-1',
             },
         });
-        return data
+        return response.data;
     }
 }
