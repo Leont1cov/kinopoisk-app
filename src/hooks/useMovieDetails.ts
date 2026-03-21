1dd10ddfabbd8e32f32e15c5416dbd6c8561b52d
Leont1cov <chuchunovvakov@mail.ru>
2026-03-21 21:23:16 +0300

    fix(types): replace any with MovieDetail interface in useMovieDetail hook

--- src/hooks/useMovieDetails.ts
+++ src/hooks/useMovieDetails.ts
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { movieService } from "../api/movieService";
 import { useParams } from "react-router-dom";
+import type { MovieDetail } from "../types/types.ts";
 
 export const useMovieDetail = () => {
-  const { id } = useParams<{ id: string }>(); // Берем ID из URL
-  const [movie, setMovie] = useState<any>(null); // Пока any, потом типизируем
+  const { id } = useParams<{ id: string }>();
+  const [movie, setMovie] = useState<MovieDetail | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -18,6 +19,7 @@ export const useMovieDetail = () => {
         setMovie(data);
       } catch (err) {
         setError("Не удалось загрузить информацию о фильме");
+        console.error(err)
       } finally {
         setIsLoading(false);
       }
